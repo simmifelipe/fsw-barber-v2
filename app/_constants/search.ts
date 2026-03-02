d0adf9ff0cd98823bad1c5176ab2136d064acf28
Felipe Simmi <simmifelipe1992@gmail.com>
2026-03-02 11:04:08 -0300

    feat: add BookingItem component

---
+++ app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  title: string
+  imageUrl: string
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    title: "Cabelo",
+    imageUrl: "/cabelo.svg",
+  },
+  {
+    title: "Barba",
+    imageUrl: "/barba.svg",
+  },
+  {
+    title: "Acabamento",
+    imageUrl: "/acabamento.svg",
+  },
+  {
+    title: "Massagem",
+    imageUrl: "/massagem.svg",
+  },
+  {
+    title: "Hidratação",
+    imageUrl: "/hidratacao.svg",
+  },
+  {
+    title: "Sobrancelha",
+    imageUrl: "/sobrancelha.svg",
+  },
+]
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,18 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import BarbershopItem from "./_components/barbershop-item"
 import { db } from "./_lib/prisma"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 const Home = async () => {
   const barbershops = await db.barberShop.findMany({})
+  const popularBarbershops = await db.barberShop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
   return (
     <div>
       {/* header */}
@@ -25,6 +30,19 @@ const Home = async () => {
             <SearchIcon />
           </Button>
         </div>
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button variant="secondary" className="gap-2" key={option.title}>
+              <Image
+                src={option.imageUrl}
+                alt={option.title}
+                width={16}
+                height={16}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
 
         <div className="relative mt-6 h-[150px] w-full">
           <Image
@@ -35,30 +53,7 @@ const Home = async () => {
           />
         </div>
 
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl font-bold">20</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
@@ -68,7 +63,27 @@ const Home = async () => {
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
+
+      <footer>
+        <Card className="rounded-none">
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2026 Copyright -{" "}
+              <span className="font-bold">FSW Barbershop</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
