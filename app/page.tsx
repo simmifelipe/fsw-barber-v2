775f52b1de7397a9f0ac936a6b7d6463ace53b31
Felipe Simmi <simmifelipe1992@gmail.com>
2026-03-02 09:17:01 -0300

    feat: add header components and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button size="icon">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Banner"
+            fill
+            className="ob rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
