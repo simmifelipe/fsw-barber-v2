d1d4a1406ea69ce059b433dcbe29d8765fe631ed
Felipe Simmi <simmifelipe1992@gmail.com>
2026-03-03 09:04:37 -0300

    chore: add NextAuth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,10 +1,17 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { quickSearchOptions } from "../_constants/search"
 import { Button } from "./ui/button"
 import { SheetContent, SheetHeader, SheetTitle, SheetClose } from "./ui/sheet"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import Image from "next/image"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -13,8 +20,33 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon size={18} />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando a conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                src="/google.svg"
+                alt="Fazer login com Google"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
         </Avatar>
         <div className="ml-3 flex flex-col">
@@ -22,7 +54,7 @@ const SidebarSheet = () => {
           <p className="text-xs text-muted-foreground">
             simmifelipe1992@gmail.com
           </p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-4 border-b border-solid py-5">
